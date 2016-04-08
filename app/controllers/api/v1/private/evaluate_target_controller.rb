2e281296e57c651c0c2f76fdbb51170ff84ce665
Yuri Kaspervich <ykas.gg@gmail.com>
2016-04-09 02:46:29 +0300

    Demeter out Country#calculate_price

--- app/controllers/api/v1/private/evaluate_target_controller.rb
+++ app/controllers/api/v1/private/evaluate_target_controller.rb
@@ -4,8 +4,6 @@ class Api::V1::Private::EvaluateTargetController < ApplicationController
   def evaluate_target
     country = Country.find_by_country_code(params[:country_code])
     
-    price = country.panel_provider.calculate_price
-    
-    render json: { price: price }.to_json
+    render json: { price: country.calculate_price }.to_json
   end
 end
