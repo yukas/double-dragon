2fd68af2fa7897c231d11bc2cac2505c7f07b413
Yuri Kaspervich <ykas.gg@gmail.com>
2016-04-09 03:38:24 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Traver.create(country: {
+  country_code:    "BY",
+  panel_provider:  { type: "LetterPanelProvider" },
+  location_groups: [{ locations: 7 }],
+  target_groups:   [:target_group_level_1]
+})
+
+Traver.create(country: {
+  country_code:    "PL",
+  panel_provider:  { type: "TagPanelProvider" },
+  location_groups: [{ locations: 7 }],
+  target_groups:   [:target_group_level_1]
+})
+
+Traver.create(country: {
+  country_code:    "GB",
+  panel_provider:  { type: "NodePanelProvider" },
+  location_groups: [2, { locations: 7 }],
+  target_groups:   [2, :target_group_level_1]
+})
--- test/factories.rb
+++ test/factories.rb
@@ -1,5 +1,22 @@
 Traver.factories do
+  factory :location, {
+    name: "Location ${n}"
+  }
+  
   factory :target_group, {
-    parent: nil
+    parent: nil,
+    name: "Target Group ${n}"
+  }
+  
+  factory :target_group_level_1, :target_group, {
+    children: [:target_group_level_2]
+  }
+  
+  factory :target_group_level_2, :target_group, {
+    children: [:target_group_level_3]
+  }
+  
+  factory :target_group_level_3, :target_group, {
+    children: [:target_group]
   }
 end
